b1e0bee80fc2d6574bca834ce0bd4f6407a285b2
Robilko <sasha.robson@gmail.com>
2021-08-01 08:40:46 +0300

    new

---
+++ p0421_simplelist/src/main/java/com/robivan/p0421_simplelist/MainActivity.java
@@ -0,0 +1,30 @@
+package com.robivan.p0421_simplelist;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // находим список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.my_list_item, names);
+
+        // присваиваем адаптер списку
+        lvMain.setAdapter(adapter);
+
+    }
+}
